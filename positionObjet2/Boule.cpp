e3763de99b68e7b16a14d74e400428a28e0f098d
Zorbass <zorbas2005@gmail.com>
2022-05-30 18:39:26 +0200

    1839

--- positionObjet2/Boule.cpp
+++ positionObjet2/Boule.cpp
@@ -25,7 +25,17 @@ void Boule::deplacement()
     cout << "Vitesse y ??" << endl;
     cin >> m_vy;
 
-    m_v = sqrt(m_vy*m_vy + m_vx*m_vx);
+    m_x = m_x + (m_vx*m_vx) / (2*m_g*m_f);
+    m_y = m_y + (m_vy*m_vy) / (2*m_g*m_f);
+
+    /*
+    m_x = m_x + m_vx * m_t + m_ax * m_t * m_t / 2;
+    m_y = m_y + m_vy * m_t + m_ay * m_t * m_t / 2;
+    */
+    /*
+    m_ax =
+    m_ay =
+    m_t =
 
     m_ax = -m_m * m_g * m_f * cos(atan((PI * m_vy/m_vx)/180));
     m_ay = -m_m * m_g * m_f * sin(atan((PI * m_vy/m_vx)/180));
@@ -34,9 +44,8 @@ void Boule::deplacement()
     cout << "temps : " << m_t << endl;
     cout << "ax : " << m_ax<< endl;
     cout << "ay : " << m_ay<< endl;
+    */
 
-    m_x = m_x + m_vx * m_t + m_ax * m_t * m_t / 2;
-    m_y = m_y + m_vy * m_t + m_ay * m_t * m_t / 2;
 }
 
 /*
