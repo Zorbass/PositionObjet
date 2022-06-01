f47586d6dac4bb1437161dde28786ca1e15d0ede
Zorbass <zorbas2005@gmail.com>
2022-06-01 17:15:10 +0200

    test

--- positionObjet2/Boule.hpp
+++ positionObjet2/Boule.hpp
@@ -20,9 +20,19 @@ class Boule
     std::string m_nom;
     double m_x;
     double m_y;
+
     double m_vx;
     double m_vy;
+
+    double m_ax;
+    double m_ay;
+
     double m_t;
+    double m_m = 0.162;
+    double m_c = 0.5;
+    double m_g = 9.81;
+    double m_f = 0.3;
+    double m_v;
 };
 
 #endif
