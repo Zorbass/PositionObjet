5c29f8f66101562f1173d699c653b8e7d665ebbf
Zorbass <zorbas2005@gmail.com>
2022-06-05 10:24:25 +0200

    asa

--- positionObjet2/Boule.cpp
+++ positionObjet2/Boule.cpp
@@ -31,5 +31,31 @@ void Boule::deplacement()
     double kx = m_vx/m_v;
     double ky = m_vy/m_v;
 
+    cout << clock() << endl;
+    while(n > i)
+    {
+        double u = clock();
+        t = clock();
+
+        while(t-u < f)
+        {
+            t = clock();
+        }
+
+       wedwdwd m_v = m_v - m_a*f*m_tx/1000/m_t;
+
+        //m_vx = m_vx - m_a*f*m_tx/1000/m_t;
+        m_x = m_x - m_a*0.5*f*f*m_tx*m_tx*m_tx/1000000/m_t/m_t/m_t + m_v*f*m_tx*m_tx/1000/m_t/m_t;
+
+        //m_vy = m_vy - m_ay*f/1000;
+        m_y = m_y - m_a*0.5*f*f*m_ty*m_ty*m_ty/1000000/m_t/m_t/m_t + m_v*f*m_ty*m_ty/1000/m_t/m_t;
+
+        /*m_x = m_x + m_vx * m_t/n;
+        m_y = m_y + m_vy * m_t/n;*/
+
+        i++;
+    }
+    cout << clock() << endl;
+
 
     }
