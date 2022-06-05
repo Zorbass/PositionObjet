12517fe77040701ba628387f167b2a5495855e88
Zorbass <zorbas2005@gmail.com>
2022-06-05 20:51:03 +0200

    asa

--- positionObjet2/main.cpp
+++ positionObjet2/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     Boule A("A", 2, 2); // ne pas mettre 0,0 sinon sa beug (le programme change son sens a l'infini)
-    Table T(0,0,11,6)
+    Table T(0,0,11,6);
     A.afficher();
     A.deplacement();
     A.afficher();
