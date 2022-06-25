80de821a3ba9ab3e291c149dd31eab88d41ac266
Zorbass <zorbas2005@gmail.com>
2022-06-25 09:29:05 +0200

    qssq

---
+++ positionObjet2/vecteur.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "Vecteur.hpp"
+
+using namespace std;
+
+Vecteur::Vecteur() : m_x(0), m_y(0)
+{
+}
+
+Vecteur::Vecteur(double x, double y) : m_x(x), m_y(y)
+{
+}
+
+
+
+void Vecteur::modifier(double x, double y)
+{
+    m_x = x;
+    m_y = y;
+}
+
+/*void Vecteur::afficher() const
+{
+    cout << "(" << m_x << ";" << m_y << ")" << endl;
+}*/
+
+void Vecteur::afficher(ostream &flux) const
+{
+    flux << "(" << m_x << ";" << m_y << ")";
+}
+
+double Vecteur::x()
+{
+    return m_x;
+}
+
+double Vecteur::y()
+{
+    return m_y;
+}
+
+
+Vecteur& Vecteur::operator+=(const Vecteur &vecteur2)
+{
+    m_x += vecteur2.m_x;
+    m_y += vecteur2.m_y;
+
+    return *this;
+}
+
+Vecteur operator+(Vecteur const& a, Vecteur const& b)
+{
+    Vecteur copie(a);
+    copie += b;
+    return copie;
+}
+
+ostream& operator<<( ostream &flux, Vecteur const& vecteur)
+{
+    vecteur.afficher(flux) ;
+    return flux;
+}
---
+++ positionObjet2/vecteur.hpp
@@ -0,0 +1,30 @@
+#ifndef Vecteur_HPP_INCLUDED
+#define Vecteur_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vecteur
+{
+public:
+    Vecteur();
+    Vecteur(double x, double y);
+    Vecteur& operator+=(const Vecteur &vecteur);
+    //void afficher() const;
+    double x();
+    double y();
+    void afficher(std::ostream &flux) const;
+    void modifier(double x, double y);
+
+private:
+    double m_x;
+    double m_y;
+
+};
+
+Vecteur operator+(Vecteur const& a, Vecteur const& b);
+ostream& operator<<(ostream &flux, Vecteur const& vecteur);
+
+#endif
