3dfc8afa71db3ef234ba1fc0cb42846dd87ee432
Zorbass <zorbas2005@gmail.com>
2022-06-27 16:46:24 +0200

    deada

--- positionObjet2/Table.hpp
+++ positionObjet2/Table.hpp
@@ -1,24 +1,52 @@
+
 #ifndef TABLE_HPP_INCLUDED
 #define TABLE_HPP_INCLUDED
 
+#include <iostream>
+#include <string>
 #include "Boule.hpp"
+#include <vector>
 
 class Table
 {
+
 public:
     Table();
-    Table(double x, double y, double x2, double y2);
-    Boule *m_boule;
+    Table(std::string nom, double x, double y);
+    ~Table();
 
 
     void regle();
     void innitialisation();
+    void test();
+
 
 private:
-    double m_x;
-    double m_y;
-    double m_x2;
-    double m_y2;
+    Boule *m_b;
+
+    int nombreDeBoules=16;
+
+
+    Boule *m_b1;
+    Boule *m_b2;
+    Boule *m_b3;
+    Boule *m_b4;
+    Boule *m_b5;
+    Boule *m_b6;
+    Boule *m_b7;
+    Boule *m_b8;
+    Boule *m_b9;
+    Boule *m_b10;
+    Boule *m_b11;
+    Boule *m_b12;
+    Boule *m_b13;
+    Boule *m_b14;
+    Boule *m_b15;
+    Boule *m_b16;
+
+
 };
 
-#endif // TABLE_HPP_INCLUDED
+
+#endif
+
