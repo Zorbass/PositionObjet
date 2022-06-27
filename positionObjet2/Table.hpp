dc0002214b410d031cc05d8488477f4780be6bb6
Zorbass <zorbas2005@gmail.com>
2022-06-27 15:19:05 +0200

    sasa

--- positionObjet2/Table.hpp
+++ positionObjet2/Table.hpp
@@ -1,6 +1,22 @@
 #ifndef TABLE_HPP_INCLUDED
 #define TABLE_HPP_INCLUDED
 
+class Table
+{
+public:
+    Table();
+    Table(int x, int y, int x2, int y2);
+    Boule *m_boule;
 
+
+    void regle();
+    void innitialisation();
+
+private:
+    int m_x;
+    int m_y;
+    int m_x2;
+    int m_y2;
+};
 
 #endif // TABLE_HPP_INCLUDED
