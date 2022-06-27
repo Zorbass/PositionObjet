5177995c09f0eecb94eeaf43af4cefb7a021f96a
Zorbass <zorbas2005@gmail.com>
2022-06-27 15:21:21 +0200

    dsfsedf

--- positionObjet2/Table.cpp
+++ positionObjet2/Table.cpp
@@ -2,3 +2,18 @@
 #include <iostream>
 
 using namespace std;
+
+using namespace std;
+
+Table::Table() : m_x(0), m_y(0), m_x2(11), m_y2(6)
+{
+}
+
+Table::Table(int x, int y, int x2, int y2) : m_x(x), m_y(y), m_x2(x2), m_y2(y2)
+{
+}
+
+void Table::innitialisation()
+{
+    cout << m_x2 << endl;
+}
